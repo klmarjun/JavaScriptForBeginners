3f00eb6a28b9f90ddde7864a3744b9a1828bf90b
Arjun M <114378622+klmarjun@users.noreply.github.com>
2023-09-02 23:51:02 +0530

    Add files via upload

---
+++ Class/Day13/Code/StringInterpolation.js
@@ -0,0 +1,5 @@
+const name = "ABC";
+let mark = 90;
+console.log(`name: ${name} and mark: ${mark}`)
+//backtics(` `) are used for template literal - String interpolation
+//string concatenation('+' ',' )
